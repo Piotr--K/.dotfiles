cba84882200654403ebde96a185d604daf117b71
Piotr K <piotrk@szary.com>
2024-10-24 20:07:09 +0100

    toggle autormatting

--- nvim/.config/nvim/lua/user/formatter.lua
+++ nvim/.config/nvim/lua/user/formatter.lua
@@ -2,18 +2,44 @@ local M = {}
 M.config = function()
     event = { "BufReadPre", "BufNewFile" }
     local conform = require("conform")
-
+    local format_on_save_enabled = true
+      
     conform.setup({
       formatters_by_ft = {
         javascript = { "prettier", "eslint_d" },
         typescript = { "prettier", "eslint_d" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 500,
-      },
+      -- format_on_save = {
+        -- lsp_fallback = true,
+        -- async = false,
+        -- timeout_ms = 500,
+      -- },
     })
+  
+    function ToggleFormatOnSave()
+      format_on_save_enabled = not format_on_save_enabled
+      
+      if format_on_save_enabled then
+        print("Auto-format on save enabled")
+      else
+        print("Auto-format on save disabled")
+      end
+    end
+
+    vim.api.nvim_create_autocmd({"BufWritePre"}, {
+      callback = function()
+        if format_on_save_enabled then
+          conform.format({
+            lsp_fallback = true,
+            async = false,
+            timeout_ms = 500,
+          })
+        end
+      end
+    })
+
+    -- Keybinding to manually toggle auto-formatting on/off
+    vim.keymap.set("n", "<leader>tf", ToggleFormatOnSave, { desc = "Toggle format on save" })
 
     vim.keymap.set({ "n", "v" }, "<leader>pp", function()
       conform.format({
